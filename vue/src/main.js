a765a01e32aa46be6f226165e6bcdf60a8207288
지형 <jihyoung.lee90@gmail.com>
2025-07-13 10:37:17 +0900

    ADD Dockerfile

--- vue/src/main.js
+++ vue/src/main.js
@@ -8,7 +8,7 @@ import './assets/tailwind.css'
 import i18n from './i18n.js'
 import GoogleLogin from 'vue3-google-login'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8081'
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 axios.defaults.withCredentials = true
 
 const pinia = createPinia()
